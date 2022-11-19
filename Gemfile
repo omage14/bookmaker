b4a85362684cc9b0f9e3cc269d9d6d051322d3d8
omage14 <omagefriday@gmail.com>
2022-11-19 09:25:08 -0600

    create user devise model and base controller

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'puma', '~> 5.0'
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
-
+gem 'devise', '~> 4.8', '>= 4.8.1'
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# User API Endpoints
+class Api::V1::UsersController < Api::V1::BaseController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
